a23b4f75cceebb2676308ca84bbbcc647c1ef7c5
Mominul Islam Rasal <mominul.rasal@bjitgroup.com>
2022-05-26 10:10:51 +0600

    4th lesson

--- lesson4/index.js
+++
@@ -1,7 +0,0 @@
-const pathToRegexp = require('path-to-regexp');
-
-
-const urlMatch = pathToRegexp.match('/student/:id');
-
-console.log(urlMatch('/student/6'));
-console.log(Object.keys(urlMatch('/student/6').params).length === 0);
